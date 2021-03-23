74aa0f080b504e6b7d2cafed84c191d2e26d0290
Gabrielvd <gabrieldeoliveira4@gmail.com>
2021-03-23 10:19:28 -0300

    desafio 57 do curso de python do curso em video

---
+++ qualSexoDaPessoa-desafio19.py
@@ -0,0 +1,5 @@
+sexo = str(input("Qual seu sexo, digite m para masculino e f para feminino: "))
+
+while sexo not in 'fmMF':
+  sexo = str(input("Dado invalido, digite m para masculino e f para feminino: "))
+print("acabou")
