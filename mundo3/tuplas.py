93854d24bfad01dc1572b537b3c6f8002269535b
Gabrielvd <gabrieldeoliveira4@gmail.com>
2022-04-08 14:59:20 -0300

    adicionado o exercicios de tuplas do mundo 3 do curso de python do gustavo guanabara

---
+++ mundo3/tuplas.py
@@ -0,0 +1,9 @@
+lanche = ('hambúrguer', 'suco', 'pizza', 'pudim')
+
+for cont in range(0, len(lanche)):
+  print(f"Eu vou comer {lanche[cont]}")
+
+for comida in lanche:
+  print(f"Eu vou comer {comida}")
+
+print("Comi pra caramba")
